76950950a620bdbc8b4fe567cd1f6647ee7c7a1e
Xay Rangsis <xrangsis@gmail.com>
2023-07-22 06:14:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ try:
 if not fruit_choice:
   streamlit.error("Please select a fruit to get information.")
 else:
-  #import requests
+  #import requestss
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # putting data into a datafram?
