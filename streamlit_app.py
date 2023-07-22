dcfe4e75250e8e17da392b8a779c4c21f62c2570
Xay Rangsis <xrangsis@gmail.com>
2023-07-21 18:29:12 -0700

    Update streamlit_app.py
    Add more titles

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Kale, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
